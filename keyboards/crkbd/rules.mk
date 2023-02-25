3295b5102da5f1e02bae31245d777610d3dff6af
chai-md <48555545+chai-md@users.noreply.github.com>
2023-02-26 02:44:18 +0900

    crkbd RGB and OLED settings

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -7,7 +7,7 @@ BOOTLOADER = caterina
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -16,9 +16,11 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
 
 # if firmware size over limit, try this option
 # LTO_ENABLE = yes
